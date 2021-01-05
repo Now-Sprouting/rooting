ee837563784d71d24aa717e94cf31b623f6a3c47
Now_Sprouting <15542456716@163.com>
2021-01-05 21:23:19 +0800

    Button 组件编写测试完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { shadesOfPurple } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 function App() {
+  const codeString = `<Button btnType={ButtonType.Default} size={ButtonSize.Large}>Default</Button>
+<Button btnType={ButtonType.Danger}>Danger</Button>
+<Button btnType={ButtonType.Primary} size={ButtonSize.Small}>Primary</Button>
+<Button btnType={ButtonType.Primary} size={ButtonSize.Small} disabled={true}>Disable</Button>
+<Button btnType={ButtonType.Link} href='http://www.baidu.com'>Link</Button>
+<Button btnType={ButtonType.Link} href='http://www.baidu.com' disabled={true}>Link</Button>`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button btnType={ButtonType.Default} size={ButtonSize.Large} onClick={(e) => {console.log(e.target)}}>Default</Button>
+      <Button btnType={ButtonType.Danger}>Danger</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>Primary</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small} disabled={true}>Disable</Button>
+      <Button btnType={ButtonType.Link} href='http://www.baidu.com'>Link</Button>
+      <Button btnType={ButtonType.Link} href='http://www.baidu.com' disabled={true}>Link</Button>
+      <div style={{width: '1000px', margin: 'auto'}}>
+        <SyntaxHighlighter language="jsx" style={shadesOfPurple}>
+          {codeString}
+        </SyntaxHighlighter>
+      </div>
+      <div onClick={(e) => {console.log(e.nativeEvent.currentTarget)}}>123</div>
     </div>
   );
 }
